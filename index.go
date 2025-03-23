8f071cd0fa8b7115abbdc1df595cc8be1f49249b
Fallah Andy Prakasa <me@flla.my.id>
2025-03-23 16:53:59 +0700

    fix: dont hardcode the title

--- index.go
+++ index.go
@@ -4,7 +4,7 @@ const indexTmpl string = `
 <!doctype html>
 <html>
   <head>
-    <title>Scalar API Reference</title>
+    <title>{{ .Title }}</title>
     <meta charset="utf-8" />
     <meta
       name="viewport"
